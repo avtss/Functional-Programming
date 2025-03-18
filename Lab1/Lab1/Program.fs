26085b1b5c1e3ff650ad183d71beba7cba39db96
avtss <avetiszadikyann@mail.ru>
2025-03-18 13:30:11 +0300

    площадь круга и обьем цилиндра с каррированием и суперпозицией

--- Lab1/Lab1/Program.fs
+++ Lab1/Lab1/Program.fs
@@ -1,8 +1,43 @@
-﻿printfn "Hello World"
+﻿//printfn "Hello World"
 
 let solve (a,b,c) =
     let D = b*b-4.*a*c
-    ((-b+sqrt(D))/(2.*a), (-b-sqrt(D))/(2.*a)) in
-        solve (1.0, 2.0,-3.0);;
+    ((-b+sqrt(D))/(2.*a), (-b-sqrt(D))/(2.*a));;
+
+let circleArea r =
+    let pi = 3.14159
+    pi * r * r
+
+let cylinderVolume r h =
+    h * circleArea r
+
+let bigCylinderVol = cylinderVolume 10
+
+let main () =
+    // суперпозиция
+    printfn "Введите радиус цилиндра:"
+    let radius = System.Console.ReadLine() |> float
+    printfn "Введите высоту цилиндра:"
+    let height = System.Console.ReadLine() |> float
+
+    let calculateVolume = circleArea >> ((*) height)
+    let volume = calculateVolume radius
+
+    printfn "Объем цилиндра (суперпозиция): %f" volume
+
+    // каррирование
+    printfn "Введите радиус цилиндра:"
+    let radius = System.Console.ReadLine() |> float
+
+    printfn "Введите высоту цилиндра:"
+    let height = System.Console.ReadLine() |> float
+
+    let calculateVolume = cylinderVolume radius 
+    let volume = calculateVolume height
+
+    printfn "Объем цилиндра (каррирование): %f" volume
+
+    System.Console.ReadKey()
 
 
+main()
--- Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.fs
+++ Lab1/Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f60a0572adfff3dd163ca27afb68b1e518adedd")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d134ea572f756a3f61580e793d6beb095f3a5ac2")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Lab1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Lab1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
