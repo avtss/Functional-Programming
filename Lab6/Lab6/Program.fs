f48b793aea699c83726be7303ad3d35666a92fd8
avtss <avetiszadikyann@mail.ru>
2025-04-01 13:15:51 +0300

    lab6.1

---
+++ Lab6/Lab6/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let readList n =
+    let rec readListRec n acc = 
+        match n with
+        |0 -> acc
+        |k ->
+            let el = Console.ReadLine() |> int
+            let newAcc = acc@[el]
+            readListRec (k-1) newAcc
+    readListRec n []
+
+let main =
+    readList 5
+
+main
